ebef1632003860e0780863af5b0b7580d55fdd80
rolando-R <landon3388@gmail.com>
2021-02-02 22:55:33 -0500

    red

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: colors.rose,
+        pink: colors.pink,
+        orange: colors.orange,
+        cyan: colors.cyan,
+        fuchsia: colors.fuchsia,
+        purple: colors.purple,
+        violet: colors.violet,
+        indigo: colors.indigo,
+        lightBlue: colors.lightBlue,
+        teal: colors.teal,
+        emerald: colors.emerald,
+        lime: colors.lime,
+        amber: colors.amber,
+        green: colors.green,
+        red: colors.red,
+
+        warmGray: colors.warmGray,
+        trueGray: colors.trueGray,
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
+        gray: colors.gray,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
