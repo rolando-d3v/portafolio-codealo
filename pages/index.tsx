2193c070f8ba37798dafec01b59e4fc3fd402b16
rolando-R <landon3388@gmail.com>
2021-02-02 21:30:37 -0500

    nuevo

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div>
+      <h2>hola mundo</h2>
+    </div>
+    </div>
+  )
+}
