d05fac49943719f108f755c24cdd667436f0bc1a
rolando-R <landon3388@gmail.com>
2021-02-03 21:52:51 -0500

    portafolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen bg-black-bg">
+      <div className="h-full bg-black-bg ">
         <Navigation />
 
         <main>
