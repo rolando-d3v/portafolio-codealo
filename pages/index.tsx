e8b082bfe52248a140693197aa5198d0386eaa31
rolando-R <landon3388@gmail.com>
2021-02-06 19:31:52 -0500

    reducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,13 @@ export default function Home() {
         <main className=" md:flex">
           <section className="w-40 px-6 hidden md:block">
             <div className="h-full flex flex-col justify-center items-center">
-              <div className="space-y-6">
+              <div className="space-y-6 flex flex-col items-center">
                 <FaIcons.FaFacebook className=" text-white w-6 h-6" />
                 <FaIcons.FaTwitter className=" text-white w-6 h-6" />
                 <FaIcons.FaInstagram className=" text-white w-6 h-6" />
               </div>
               <div className="h-32"></div>
-              <div className="space-y-6">
+              <div className="space-y-6 flex flex-col items-center">
                 <span className=" bg-white w-5 h-5 block rounded-full "></span>
                 <span className=" bg-white w-4 h-4 block rounded-full "></span>
                 <span className=" bg-white w-3 h-3 block rounded-full "></span>
@@ -36,29 +36,29 @@ export default function Home() {
           <section className="p-10 flex items-center">
             <div className="">
               <h2
-                className="text-white font-bold text-5xl xl:text-6xl mb-8 "
+                className="text-white font-bold text-5xl xl:text-7xl mb-8 "
                 style={{ textShadow: "0px 10px 50px rgba(255,255,255,0.45)" }}
               >
                 Bienvenidos al blog de Rolando
               </h2>
               <hr className="border border-primary hidden md:flex" />
-              <p className="text-gray-400 text-lg mt-8 ">
+              <p className="text-gray-400  text-lg  lg:text-xl mt-8 ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
                 laudantium molestias, nulla provident dolore nisi ducimus
                 voluptatibus laborum eveniet quo quos cum eaque repellat quas
                 ad. Eius ducimus rerum harum.
               </p>
 
-              <div className="hidden md:block my-4">
-                <h3 className="text-white">Regsitrate para mas contenido</h3>
+              <div className="hidden md:block mt-8 w-full">
+                <h3 className="text-white text-xl font-bold mb-6 ">Regsitrate para mas contenido</h3>
                 <div className="flex space-x-3">
                   <input
-                    className="rounded-2xl px-4 focus:outline-none bg-gray-300 bg-opacity-25 text-white"
+                    className="rounded-2xl w-9/12 lg:w-7/12 px-4 py-3 focus:outline-none bg-gray-300 bg-opacity-25 text-white text-xl"
                     type="text"
                     placeholder="Ingresa tu email"
                   />
-                  <div className="bg-primary h-10 w-10 rounded-full flex items-center">
-                    <FaIcons.FaTelegramPlane className=" text-white w-6 h-6 ml-1.5 " />
+                  <div className="bg-primary h-14 w-14 rounded-full flex items-center">
+                    <FaIcons.FaTelegramPlane className=" text-white w-8 h-8 ml-2 " />
                   </div>
                 </div>
               </div>
